d060a00ec65acecd96cd45f4eaad974db398499c
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-10-18 16:10:31 +0200

    test: json to typebox cli

---
+++ json2typebox-cli.spec.ts
@@ -0,0 +1,14 @@
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
+
+describe("json2typebox-cli", () => {
+  it("should transform json object into typebox definition format and create a new file with the result of the transformation", () => {
+    execSync(
+      "npx tsx ./json2typebox-cli.ts ./data-test/1.json ./data-test/1.ts"
+    );
+    const writtenSchema = readFileSync("./data-test/1.ts", "utf-8");
+    const expectedSchema = `import { Type } from '@sinclair/typebox'
+ export const LwM2M_Server = Type.Object({Short_Server_ID: Type.Number({$id: '0', description: "Used as link to associate server Object Instance.", minimun: 1, maximun: 65534})}, {description: "This LwM2M Objects provides the data related to a LwM2M Server. A Bootstrap-Server has no such an Object Instance associated to it."})`;
+    expect(writtenSchema).toEqual(expectedSchema);
+  });
+});
