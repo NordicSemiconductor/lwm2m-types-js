207ff67e19c537bc3905bbba0b8c92da29227ae8
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-10-18 13:32:11 +0200

    fix: simplify xml to json process

--- xml2json.spec.ts
+++ xml2json.spec.ts
@@ -1,52 +1,54 @@
-import { getXml, createJson, main } from "./xml2json";
+import { execSync } from "child_process";
+import { readFileSync } from "fs";
 
-//TODO: rename => xml2json
-describe("xml2js", () => {
-  describe("getXml", () => {
-    it("should load a xml schema", () => {
-      const dir = "/home/Documents/";
-      const docName = "something";
-      const readXML = jest.fn();
-      getXml(dir, docName)(readXML);
-      expect(readXML).toBeCalledWith(`${dir}/${docName}.xml`);
-    });
-  });
-
-  describe("createJson", () => {
-    it("should inform if there is an error", () => {
-      const error: Error = { name: "error", message: "error message" };
-      const result = "";
-      const dir = "/home/Documents/";
-      const docName = "something";
-      expect(() => createJson(error, result, dir, docName)()).toThrow(
-        error.message
-      );
-    });
-
-    it("should create a json file", () => {
-      const error = null;
-      const result = "{test: 123}";
-      const dir = "/home/Documents/";
-      const docName = "something";
-      const writeJson = jest.fn();
-
-      createJson(error, result, dir, docName)(writeJson);
-      expect(writeJson).toBeCalledWith(
-        `${dir}/${docName}.json`,
-        JSON.stringify(result)
-      );
-    });
-  });
-
-  describe("main", () => {
-    it("should translate xml to json", () => {
-      const uploadDir = "/home/Documents/";
-      const docName = "something";
-      const writeDir = "/home/Documents/";
-      const xml2json = jest.fn();
-
-      main(uploadDir, docName, writeDir)()(xml2json);
-      expect(xml2json).toBeCalledTimes(1);
-    });
+describe("xml2json", () => {
+  it("should transform xml schema into json format and create a new file with the result of the transformation", () => {
+    execSync("npx tsx ./xml2json.ts ./data-test/1.xml ./data-test/1.json");
+    const writtenSchema = readFileSync("./data-test/1.json", "utf-8");
+    const expectedSchema = {
+      LWM2M: {
+        ATTR: {
+          "xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+          "xsi:noNamespaceSchemaLocation":
+            "http://www.openmobilealliance.org/tech/profiles/LWM2M-v1_1.xsd",
+        },
+        Object: [
+          {
+            ATTR: { ObjectType: "MODefinition" },
+            Name: ["LwM2M Server"],
+            Description1: [
+              "This LwM2M Objects provides the data related to a LwM2M Server. A Bootstrap-Server has no such an Object Instance associated to it.",
+            ],
+            ObjectID: ["1"],
+            ObjectURN: ["urn:oma:lwm2m:oma:1:1.2"],
+            LWM2MVersion: ["1.2"],
+            ObjectVersion: ["1.2"],
+            MultipleInstances: ["Multiple"],
+            Mandatory: ["Mandatory"],
+            Resources: [
+              {
+                Item: [
+                  {
+                    ATTR: { ID: "0" },
+                    Name: ["Short Server ID"],
+                    Operations: ["R"],
+                    MultipleInstances: ["Single"],
+                    Mandatory: ["Mandatory"],
+                    Type: ["Integer"],
+                    RangeEnumeration: ["1..65534"],
+                    Units: [""],
+                    Description: [
+                      "Used as link to associate server Object Instance.",
+                    ],
+                  },
+                ],
+              },
+            ],
+            Description2: [""],
+          },
+        ],
+      },
+    };
+    expect(JSON.parse(writtenSchema)).toEqual(expectedSchema);
   });
 });
--- xml2json.ts
+++ xml2json.ts
@@ -1,50 +1,21 @@
-import xml2js from "xml2js";
-import fs from "fs";
-import config from "./config/config";
-const parser = new xml2js.Parser({ attrkey: "ATTR" });
+import { readFile, writeFile } from "fs/promises";
+import { Parser } from "xml2js";
+const parser = new Parser({ attrkey: "ATTR" });
 
-/**
- * load xml schema
- */
-export const getXml =
-  (dir: string, name: string) =>
-  (read = (route: string) => fs.readFileSync(route, "utf8")): string =>
-    read(`${dir}/${name}.xml`);
+// First command line argument
+const xmlFile = process.argv[process.argv.length - 2];
 
-/**
- * Check if error and if not create a json file
- * @param error
- * @param result
- */
-export const createJson =
-  (error: Error | null, result: any, dir: string, name: string) =>
-  (
-    write = (route: string, json: string) => fs.writeFileSync(route, json)
-  ): void => {
-    if (error === null) {
-      console.log(result);
-      write(`${dir}/${name}.json`, JSON.stringify(result));
-    } else {
-      console.log(error);
-      throw new Error(error.message);
-    }
-  };
+// Second command line argument
+const jsonPath = process.argv[process.argv.length - 1];
 
-/**
- * Take a xml schema and 'translate' it to json format.
- * Then write the result in a json file
- */
-export const main =
-  (uploadDir: string, name: string, writeDir: string) =>
-  (
-    xmlFile = () => getXml(uploadDir, name)(),
-    writeJson = (error: Error | null, result: any) =>
-      createJson(error, result, writeDir, name)()
-  ) =>
-  (
-    xml2json = () =>
-      parser.parseString(xmlFile(), (error, result) => writeJson(error, result))
-  ) =>
-    xml2json();
+export const xml2json = async () => {
+  const xmlSource = await readFile(xmlFile, "utf-8");
 
-main(config.COPY_DIR, "1", config.PASTE_DIR)()();
+  const convertedSchema = await parser.parseStringPromise(xmlSource);
+
+  await writeFile(jsonPath, JSON.stringify(convertedSchema), "utf-8");
+};
+
+xml2json();
+// FIXME: remove function and allow top level await.
+// error message: Top-level await is currently not supported with the "cjs" output format
