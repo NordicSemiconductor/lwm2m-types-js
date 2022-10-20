5ef4fe57902717acba705ed9c30fddd7e6fb41e8
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-10-20 13:20:10 +0200

    test: validate json schema

--- validateWithJsonSchema.spec.ts
+++ validateWithJsonSchema.spec.ts
@@ -1,22 +1,33 @@
+import { TSchema } from "@sinclair/typebox";
 import { validateWithJSONSchema } from "./validateWithJsonSchema";
-import { LwM2M_Server } from "./data-test/1";
-
-const typedInputSchema = LwM2M_Server;
 
 describe("validateWithJSONSchema", () => {
   describe("it should validate", () => {
-    const v = validateWithJSONSchema(typedInputSchema);
-    it("Should check input is valid", () => {
-      const isValid = v({ Short_Server_ID: 42 });
+    let LwM2M_Server: TSchema | undefined = undefined;
+    let v: ReturnType<typeof validateWithJSONSchema> | undefined = undefined;
+
+    beforeAll(async () => {
+      const generatedTypes = await import("./data-test/1");
+      v = validateWithJSONSchema(generatedTypes.LwM2M_Server);
+    });
+
+    it("Should check input is valid", async () => {
+      const isValid = (v as ReturnType<typeof validateWithJSONSchema>)({
+        Short_Server_ID: 42,
+      });
       expect("value" in isValid).toEqual(true);
       expect((isValid as any).value.Short_Server_ID).toEqual(42);
     });
     it("Should check as 'invalid' values less than 0", () => {
-      const isInvalid = v({ cell: -42 });
+      const isInvalid = (v as ReturnType<typeof validateWithJSONSchema>)({
+        Short_Server_ID: -42,
+      });
       expect("errors" in isInvalid).toEqual(true);
     });
-    it("Should check as 'invalid' values greater than 65534", () => {
-      const isInvalid = v({ cell: 655340 });
+    it("Should check as invalid' values greater than 65534", () => {
+      const isInvalid = (v as ReturnType<typeof validateWithJSONSchema>)({
+        Short_Server_ID: 655340,
+      });
       expect("errors" in isInvalid).toEqual(true);
     });
   });
