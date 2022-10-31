4f2853d0d41f8966f77e9f19e88bdbb3a978b084
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-10-31 11:11:36 +0100

    fix: display directory and documents

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { execSync } from "child_process";
 
 execSync("npx tsx ./src/pre-steps");
+execSync("cd ./LWM2M/XML/lwm2m-registry && ls");
 execSync("npx tsx ./src/XML/index");
 execSync("npx tsx ./src/Json/index");
 execSync("npx tsx ./src/TypeScript/createTypeboxDef");
