6115fe290b8a19f9ded6d93408e933512ecbfba0
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2022-11-01 10:48:31 +0100

    fix: replace bash magic with Node magic

--- cli/JSONtoTypeBox.ts
+++ cli/JSONtoTypeBox.ts
@@ -1,13 +1,16 @@
 import path from "node:path";
 import { readFile, writeFile } from "fs/promises";
 import { createDefinition } from "../src/Json/json2typebox";
+import { readdirSync } from "fs";
 
-const [, , ...objectFiles] = process.argv;
-const outDir = objectFiles.pop();
+const inputDir = process.argv[process.argv.length - 2];
+const outDir = process.argv[process.argv.length - 1];
 
-for (const file of objectFiles
-  // only process object JSON files
-  .filter((f) => /^[0-9]+\.json$/.test(path.parse(f).base))) {
+const objectFiles = readdirSync(inputDir).filter((f) =>
+  /^[0-9]+\.json$/.test(path.parse(f).base)
+);
+
+for (const file of objectFiles.map((f) => path.join(inputDir, f))) {
   const sourceFile = path.join(process.cwd(), file);
   const json = JSON.parse(await readFile(sourceFile, "utf-8"));
   const outFile = path.join(
--- cli/XMLtoJSON.ts
+++ cli/XMLtoJSON.ts
@@ -1,14 +1,17 @@
 import path from "node:path";
 import { readFile, writeFile } from "fs/promises";
+import { readdirSync } from "fs";
 import { Parser } from "xml2js";
 const parser = new Parser({ attrkey: "ATTR" });
 
-const [, , ...objectFiles] = process.argv;
-const outDir = objectFiles.pop();
+const inputDir = process.argv[process.argv.length - 2];
+const outDir = process.argv[process.argv.length - 1];
 
-for (const file of objectFiles
-  // only process object XML files
-  .filter((f) => /\.xml$/.test(path.parse(f).base))) {
+const xmlFiles = readdirSync(inputDir).filter((f) =>
+  /\.xml$/.test(path.parse(f).base)
+);
+
+for (const file of xmlFiles.map((file) => path.join(inputDir, file))) {
   const sourceFile = path.join(process.cwd(), file);
   const xml = await readFile(sourceFile, "utf-8");
   const convertedSchema = await parser.parseStringPromise(xml);
@@ -17,6 +20,6 @@ for (const file of objectFiles
     outDir as string,
     `${path.parse(sourceFile).name}.json`
   );
-  console.log(sourceFile, "->", outFile);
+  //console.log(sourceFile, "->", outFile);
   await writeFile(outFile, JSON.stringify(convertedSchema), "utf-8");
 }
