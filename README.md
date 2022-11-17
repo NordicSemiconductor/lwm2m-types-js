7be96ffa53f3a8b71d565ad57ec617acaed1f398
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-11-17 09:48:01 +0100

    docs: link to example

--- README.md
+++ README.md
@@ -30,19 +30,7 @@ npx tsx ./example.ts
 
 ## Usage
 
-```typescript
-// This is a JSON representation of LwM2M, for example stored in AWS IoT Shadow
-// The JSON notation for LwM2M follows previous work by AVSystem as used in Coiote, but with values follow the LwM2M standard (e.g. numbers are expressed as strings in Coiote, but are Integers in LwM2M standard)
-import stateJSON from "./data.json";
-// We can validate that the data is correctly defined as LwM2M
-const maybeValidLwM2M = validateLwM2MJSON(stateJSON.state.reported);
-
-if ("error" in maybeValidLwM2M) throw new Error(`Validation failed`);
-
-// then we can access LwM2M objects in the shadow
-maybeValidLwM2M?.["LwM2M Server"]; // typed as LwM2MServer | undefined
-console.log(validateShadow?.["LwM2M Server"][0].Lifetime); // 43200, typeof number
-```
+See [example.ts](./example.ts)
 
 ## JSON document
 
--- example.ts
+++ example.ts
@@ -1,10 +1,12 @@
 import * as assert from "node:assert/strict";
+// This is a JSON representation of LwM2M, for example stored in AWS IoT Shadow
 import knowGoodShadow from "./known-good-shadow.json";
 import { LwM2MDocument } from "@nordicsemiconductor/lwm2m-typebox-types";
 import { validateWithJSONSchema } from "./src/utils/validateWithJsonSchema";
 
 // validate shadow
 const validateLwM2MJSON = validateWithJSONSchema(LwM2MDocument);
+// We can validate that the data is correctly defined as LwM2M
 const maybeValidLwM2M = validateLwM2MJSON(knowGoodShadow);
 
 // Because is is know to be good, there must be no errors
@@ -16,11 +18,9 @@ if ("errors" in maybeValidLwM2M) {
 // then we can access LwM2M objects in the shadow document
 const shadow = maybeValidLwM2M.value;
 
-// Ensure values are as expected
+// and we can access LwM2M objects in the shadow
 assert.deepStrictEqual(shadow["1:1.2@1.2"]?.[0][1], 43200);
 assert.deepStrictEqual(shadow["3:1.2@1.1"]?.[1], "thingy91_nrf9160");
 assert.deepStrictEqual(shadow["4:1.3@1.1"]?.[7]?.[0], "ibasis.iot");
 assert.deepStrictEqual(shadow["3303:1.1"]?.[0][5701], "Celsius degrees");
 assert.deepStrictEqual(shadow["3323:1.1"]?.[0][5602], 98.24);
-
-console.log("Validation completed successfully");
