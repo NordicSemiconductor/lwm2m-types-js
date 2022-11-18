d48b1cf65ae931a7efeb9967a1d6418f990d7b76
Markus Tacker <Markus.Tacker@NordicSemi.no>
2022-11-18 16:02:15 +0100

    feat: add (failing) example

--- src/lwm2m-to-JSON-schema/example.ts
+++ src/lwm2m-to-JSON-schema/example.ts
@@ -1,15 +1,25 @@
-/* eslint-disable */
-import { readFile } from 'fs/promises'
+import chalk from 'chalk'
+import { readdir, readFile } from 'fs/promises'
 import path from 'node:path'
 import { Parser } from 'xml2js'
 import { LwM2MJSONfromXML2js } from '../xml2js-to-plain/LwM2MJSONfromXML2js'
 import { lwM2MDefinitionToJSONSchema } from './lwM2MDefinitionToJSONSchema'
-
 const parser = new Parser({ attrkey: 'ATTR' })
-const sourceFile = path.join(process.cwd(), 'lwm2m-registry', '3427.xml')
-const xml = await readFile(sourceFile, 'utf-8')
-const xmlAsJSON = await parser.parseStringPromise(xml)
-const lwm2mDefinition = LwM2MJSONfromXML2js(xmlAsJSON)
 
-const typeBoxDef = lwM2MDefinitionToJSONSchema(lwm2mDefinition)
-console.log(JSON.stringify(typeBoxDef, null, 2))
+try {
+	for (const f of (await readdir(path.join(process.cwd(), 'lwm2m-registry')))
+		.filter((f) => /^[\d]+\.xml$/.test(f))
+		.filter((f) => !['0.xml'].includes(f))) {
+		const sourceFile = path.join(process.cwd(), 'lwm2m-registry', f)
+		const xml = await readFile(sourceFile, 'utf-8')
+		const xmlAsJSON = await parser.parseStringPromise(xml)
+
+		console.log(chalk.yellow(`Parsing ${sourceFile} ...`))
+
+		const lwm2mDefinition = LwM2MJSONfromXML2js(xmlAsJSON)
+		const typeBoxDef = lwM2MDefinitionToJSONSchema(lwm2mDefinition)
+		console.log(JSON.stringify(typeBoxDef, null, 2))
+	}
+} catch (err) {
+	console.error(chalk.red(err))
+}
