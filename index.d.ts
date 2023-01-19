fb3339c3de9ba3c5d973143b25e736bee51cf935
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-01-19 16:34:42 +0100

    fix: export type for LwM2MDocumentSchema

--- index.d.ts
+++ index.d.ts
@@ -2,3 +2,12 @@ export * from './src/type-generation/createURN'
 export * from './src/validate'
 export * from './types/LwM2M'
 export * from './types/LwM2MDocument'
+
+export declare const LwM2MDocumentSchema: {
+	$schema: 'http://json-schema.org/draft-07/schema#'
+	type: 'object'
+	$id: string
+	title: string
+	description: string
+	properties: Record<string, any>
+}
