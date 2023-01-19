84ede467716ed02f82a3314bdd8883d736caff1a
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-01-19 11:23:10 +0100

    feat: export schema

--- export.js
+++ export.js
@@ -2,3 +2,5 @@ export * from './dist/type-generation/createURN.js'
 export * from './dist/validate.js'
 export * from './types-dist/LwM2M.js'
 export * from './types-dist/LwM2MDocument.js'
+import schema from './LwM2MDocument.schema.json' assert { type: 'json' }
+export const LwM2MDocumentSchema = schema
