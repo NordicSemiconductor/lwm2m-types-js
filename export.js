09f330ed7a3de8dba4bd41f083518bf918328792
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-04-26 13:05:38 +0200

    fix: export parseURN method

--- export.js
+++ export.js
@@ -4,3 +4,4 @@ export * from './types-dist/LwM2M.js'
 export * from './types-dist/LwM2MDocument.js'
 import schema from './LwM2MDocument.schema.json' assert { type: 'json' }
 export const LwM2MDocumentSchema = schema
+export { parseURN } from './src/type-generation/parseURN.js'
