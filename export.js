ffad585065b113ca437c65e957fc682e11cd023f
Mauro López <lopezjimenezmauro05@gmail.com>
2023-05-16 15:13:29 +0200

    fix: update import url

--- export.js
+++ export.js
@@ -1,6 +1,6 @@
 export * from './dist/type-generation/createURN.js'
 export * from './dist/validate.js'
-export { parseURN } from './src/type-generation/parseURN.js'
+export * from './dist/type-generation/parseURN.js'
 export * from './types-dist/LwM2M.js'
 export * from './types-dist/LwM2MDocument.js'
 import schema from './LwM2MDocument.schema.json' assert { type: 'json' }
