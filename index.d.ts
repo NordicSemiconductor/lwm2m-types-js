bd21e24c9e41f4c0e44b9201dc6436e69851bf7e
Mauro López <lopezjimenezmauro05@gmail.com>
2023-05-31 14:16:35 +0200

    fix: add missing file extension

--- index.d.ts
+++ index.d.ts
@@ -1,8 +1,8 @@
-export * from './src/type-generation/createURN'
-export * from './src/type-generation/parseURN'
-export * from './src/validate'
-export * from './types/LwM2M'
-export * from './types/LwM2MDocument'
+export * from './src/type-generation/createURN.js'
+export * from './src/type-generation/parseURN.js'
+export * from './src/validate.js'
+export * from './types/LwM2M.js'
+export * from './types/LwM2MDocument.js'
 
 export declare const LwM2MDocumentSchema: {
 	$schema: 'http://json-schema.org/draft-07/schema#'
