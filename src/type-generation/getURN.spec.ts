4dda28c343cf53849bb842ce26d14158c575a515
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-19 12:33:42 +0200

    refactor: rename method

--- src/type-generation/getURN.spec.ts
+++ src/type-generation/getURN.spec.ts
@@ -5,21 +5,21 @@ import {
 	LwM2MServer_1_urn,
 	Temperature_3303_urn,
 } from '../../types/LwM2M.js'
-import { getUrn } from './getURN.js'
+import { getURN } from './getURN.js'
 
-describe('getUrn', () => {
+describe('getURN', () => {
 	it('Should return the object URN given the object id', async () => {
-		expect(await getUrn('3')).toBe(Device_3_urn)
-		expect(await getUrn('10256')).toBe(
+		expect(await getURN('3')).toBe(Device_3_urn)
+		expect(await getURN('10256')).toBe(
 			ECID_SignalMeasurementInformation_10256_urn,
 		)
-		expect(await getUrn('3303')).toBe(Temperature_3303_urn)
-		expect(await getUrn('4')).toBe(ConnectivityMonitoring_4_urn)
-		expect(await getUrn('1')).toBe(LwM2MServer_1_urn)
+		expect(await getURN('3303')).toBe(Temperature_3303_urn)
+		expect(await getURN('4')).toBe(ConnectivityMonitoring_4_urn)
+		expect(await getURN('1')).toBe(LwM2MServer_1_urn)
 	})
 
 	it('Should return undefined if object is not found by the given id', async () => {
-		expect(await getUrn('40404')).toBe(undefined)
-		expect(await getUrn('-1')).toBe(undefined)
+		expect(await getURN('40404')).toBe(undefined)
+		expect(await getURN('-1')).toBe(undefined)
 	})
 })
--- src/type-generation/getURN.ts
+++ src/type-generation/getURN.ts
@@ -1,7 +1,7 @@
 /**
  * Given the object id of a LwM2M element should return its URN as it is defined in src/type-generation/createURN.ts
  */
-export const getUrn = async (objectId: string): Promise<string | undefined> => {
+export const getURN = async (objectId: string): Promise<string | undefined> => {
 	const url = `../../types/${objectId}.js`
 	const urn = await import(url)
 		.then(({ objectURN }) => objectURN)
