6981aeb6988267e25c9db4aeb44895771f3da83a
Mauro López Jiménez <mauro.lopez@nordicsemi.no>
2023-06-21 09:56:51 +0200

    fix: update path in getURN method

--- src/type-generation/getURN.ts
+++ src/type-generation/getURN.ts
@@ -2,7 +2,7 @@
  * Given the object id of a LwM2M element should return its URN as it is defined in src/type-generation/createURN.ts
  */
 export const getURN = async (objectId: string): Promise<string | undefined> => {
-	const url = `../../types/${objectId}.js`
+	const url = `./../../types-dist/${objectId}.js`
 	const urn = await import(url)
 		.then(({ objectURN }) => objectURN)
 		.catch(() => undefined)
