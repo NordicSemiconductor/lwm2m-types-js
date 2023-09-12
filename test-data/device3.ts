95b47dcb339b04d78312a9d81e6987cba5f31688
Markus Tacker <Markus.Tacker@NordicSemi.no>
2023-09-12 15:57:25 +0200

    test: remove code that was not testing anything

--- test-data/device3.ts
+++ test-data/device3.ts
@@ -1,8 +1,7 @@
-import type { Device_3 } from './types/3.js'
+// Used for bug fixed in https://github.com/NordicSemiconductor/lwm2m-types-js/pull/81
 
-/**
- * Check if created type is valid given a known valid object
- */
+import type { Device_3 } from '@nordicsemiconductor/lwm2m-types'
+
 const device: Device_3 = {
 	'0': 'Nordic Semiconductor ASA',
 	'1': 'Thingy:91',
@@ -14,6 +13,5 @@ const device: Device_3 = {
 	'16': 'UQ',
 	'19': '3.2.1',
 }
-console.log(device)
 
-// File should compile using npx tsx
+void device
